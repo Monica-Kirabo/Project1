c084bfebf6391b65836d453982b07cea4329c105
monica kirabo <kirabom@duq.edu>
2022-06-18 20:58:58 -0700

    commit here

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 const movieSearchUrl = "https://api.themoviedb.org/3/search/movie"
 const nowPlayingUrl = "https://api.themoviedb.org/3/search/movie/now_playing";
 const movieImageUrl = "https://api.themoviedb.org/3/movie";
-const apiKey='26a6ca6a064e61eeb4b31ebf6a5a3c01';
+const apiKey="26a6ca6a064e61eeb4b31ebf6a5a3c01";
+//const apiUrl=" https://api.themoviedb.org/3/search/movie/now_playing"
 const movieArea= document.getElementById('movieArea');
 var currentSearchTerm='';
 const searchInput = document.getElementById('search-input');
 const searchForm = document.getElementById('search-form');
+const IMGPATH= "https://image.tmdb.org/t/p/w1280";
 /*
 window.onload=function(){
 const moviedisplay=document.querySelector('movieArea');
@@ -16,7 +18,7 @@ const moviedisplay=document.querySelector('movieArea');
         nowplaying(dis);
     });
 } 
-*/
+
 function nowplaying(nowPlayingUrl){
 var result=fetch(nowPlayingUrl + "?api_key=" + apikey).then(response => response.json())
   .then(data => console.log(data));
@@ -71,4 +73,38 @@ function generateMore(movie){
 
 }*/
 
+nowplayingmovies(nowPlayingUrl);
+async function nowplayingmovies(url){
+    const resp=await fetch(url);
+    const respData=await resp.json();
+    console.log(respData);
+    displaymovies(respData.results);
+}
 
+function displaymovies(movies){
+   // movieArea.innerHTML=" ";
+    movies.foreach((movie)=>{
+        const{poster_path, title, vote_average, overview}=movie;
+
+        const movieel=document.createElement("div");
+        movieel.classList.add("movie");
+        movieel.innerHTML=`<img src="${IMGPATH+poster_path}" alt="${title}" /> 
+        <div class="movie-info">
+        <h3>${title}</h3>
+        <div class="overview">
+        <h3>overview </h3>
+        ${overview}
+        </div>`;
+        movieArea.appendChild(movieel);
+
+    });
+}
+searchForm.addEventListener("submit",(e)=>{
+e.preventDefault();
+const searchTerm=searchInput.value;
+if (searchTerm){
+    nowplayingmovies(movieSearchUrl+searchTerm);
+    searchInput.value=" ";
+}
+}
+);
